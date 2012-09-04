add715518e6cad662838a2f1915ee7c85565c5ca
Preston Mueller <skierjunk@gmail.com>
2012-09-04 10:06:04 -0600

    DerpyAI: Started working on our Move class located in the DerpyAI package. Not done yet

---
+++ src/DerpyAI/Move.java
@@ -0,0 +1,34 @@
+package DerpyAI;
+import java.awt.Point;
+
+import sharedfiles.*;
+
+public class Move {
+
+	public Piece pieceInvolved;
+	public Point originalLocation;
+	public Point newLocation;
+
+	private boolean pieceWasTaken;
+	private Piece pieceTaken;
+
+	public Move(Piece pieceInvolved, Point originalLocation, Point newLocation) {
+
+		this.pieceInvolved = pieceInvolved;
+		this.originalLocation = originalLocation;
+		this.newLocation = newLocation;
+	}
+
+	public void setPieceWasTaken(Piece pieceTaken) {
+
+		this.pieceTaken = pieceTaken;
+		this.pieceWasTaken = true;
+
+	}
+
+	public boolean pieceWasTaken() {
+		return pieceWasTaken;
+	}
+
+
+}
