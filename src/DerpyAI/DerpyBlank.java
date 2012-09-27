f67cbc0a8009b164669d0479da08d7a556e45f57
cwilson14 <cwilson14@192.168.1.146>
2012-09-27 14:10:10 -0600

    apparently I need to commit to fix the "WWX" thing....

--- src/DerpyAI/DerpyBlank.java
+++ src/DerpyAI/DerpyBlank.java
@@ -5,7 +5,7 @@ import java.awt.Point;
 public class DerpyBlank extends DerpyPiece{
 
 	public DerpyBlank(Point p) {
-		super(true,"WX");
+		super(true,"X");
 		currentLocation = p;
 		xMoveConstraint = 8;
 		yMoveConstraint = 8;
