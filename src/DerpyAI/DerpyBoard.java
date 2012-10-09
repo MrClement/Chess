f84be0c9d9d5b2737ddd16399fb744f0f790cb49
Preston Mueller <skierjunk@gmail.com>
2012-10-09 13:31:30 -0600

    Added DerpyBoard constructor that takes a standard Board

--- src/DerpyAI/DerpyBoard.java
+++ src/DerpyAI/DerpyBoard.java
@@ -2,9 +2,11 @@ package DerpyAI;
 
 import java.awt.Point;
 
+import sharedfiles.*;
+
 
 public class DerpyBoard {
-	
+
 	protected DerpyPiece[][] arr;
 	public DerpyBoard(){
 		arr=new DerpyPiece[8][8];
@@ -17,37 +19,81 @@ public class DerpyBoard {
 			arr[x][1] = new DerpyPawn(false, new Point(x,1));
 			arr[x][6] = new DerpyPawn(true, new Point(x,6));
 		}
-	
+
 		//black setup
 		arr[0][0] = new DerpyRook(false, new Point(0,0));
 		arr[7][0] = new DerpyRook(false, new Point(7,0));
-		
+
 		arr[1][0] = new DerpyKnight(false, new Point(1,0));
 		arr[6][0] = new DerpyKnight(false, new Point(6,0));
-		
+
 		arr[2][0] = new DerpyBishop(false, new Point(2,0));
 		arr[5][0] = new DerpyBishop(false, new Point(5,0));
-	
+
 		arr[3][0] = new DerpyQueen(false, new Point(3,0));
 		arr[4][0] = new DerpyKing(false, new Point(4,0));
-	
+
 		//white setup
 		arr[0][7] = new DerpyRook(true, new Point(0,7));
 		arr[7][7] = new DerpyRook(true, new Point(7,7));
-		
+
 		arr[1][7] = new DerpyKnight(true, new Point(1,7));
 		arr[6][7] = new DerpyKnight(true, new Point(6,7));
-		
+
 		arr[2][7] = new DerpyBishop(true, new Point(2,7));
 		arr[5][7] = new DerpyBishop(true, new Point(5,7));
-	
+
 		arr[3][7] = new DerpyQueen(true, new Point(3,7));
 		arr[4][7] = new DerpyKing(true, new Point(4,7));
 
 	}
-	
+
 	public DerpyBoard(DerpyBoard b) {
 		this.arr = b.arr;
+	}
+
+	public DerpyBoard(Board b) {
+		
+		arr=new DerpyPiece[8][8];
+		
+		Piece[][] pieces = b.getBoardArray();
+		for(int x = 0; x < 8; x++) {
+			for(int y = 0; y < 8; y++) {
+				Piece p = pieces[x][y];
+				boolean pieceColor = p.getColor();
+				
+				if(p instanceof Bishop) {
+					DerpyBishop db = new DerpyBishop(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof Blank) {
+					DerpyBlank db = new DerpyBlank(new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof King) {
+					DerpyKing db = new DerpyKing(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof Knight) {
+					DerpyKnight db = new DerpyKnight(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof Pawn) {
+					DerpyPawn db = new DerpyPawn(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof Queen) {
+					DerpyPawn db = new DerpyPawn(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+				else if(p instanceof Rook) {
+					DerpyRook db = new DerpyRook(pieceColor, new Point(x,y));
+					arr[x][y] = db;
+				}
+
+			}
+		}
+
 	}
 
 	public DerpyPiece[][] getBoardArray(){
@@ -56,24 +102,24 @@ public class DerpyBoard {
 
 	public void printBoard(){ 
 		for(int y=0; y<8; y++){
-	
+
 			for(int x=0; x<8; x++){
 				System.out.print(arr[x][y].toString()+" | ");
 			}
-		
+
 			System.out.println();
 		}
 	}
-	
+
 	public static void main (String args[]){
 		DerpyBoard a = new DerpyBoard();
 		a.printBoard();
 	}
 
 	public void updateLocations() {
-		
+
 		DerpyPiece newArr[][] = new DerpyPiece[8][8];
-		
+
 		for(int i = 0; i < 8; i++) {
 			for(int a = 0; a < 8; a++) {
 				DerpyPiece piece = arr[i][a];
@@ -82,9 +128,9 @@ public class DerpyBoard {
 				newArr[(int) location.getX()][(int) location.getY()] = piece;
 			}
 		}
-		
+
 		arr = newArr;
-		
+
 	}
-	
+
 }
