126e594856ab29db78885b4214c13bf0841f6ea4
sammathews <sammathews@192.168.0.142>
2012-10-09 19:34:59 -0600

    Minor changes

--- src/DerpyAI/Driver.java
+++ src/DerpyAI/Driver.java
@@ -8,7 +8,7 @@ public class Driver {
 		//October 7 2012: This needs to stay like this, folks! An AI is created, it's passed board, makes a move, returns the board, and prints it
 		
 		DerpyBoard db = new DerpyBoard();
-		DerpyAI aiOne = new DerpyAI(false);
+		DerpyAI aiOne = new DerpyAI(true);
 
 		db = aiOne.makeMove(db);
 
