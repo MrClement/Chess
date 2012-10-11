39b89e5486501af6f2dd23a3e1e6018373988f91
MrClement <aclement@kentdenver.org>
2012-10-11 12:30:28 -0600

    Added boardBuilder to board.  This takes a String representation of a board and updates the piece array.

--- src/sharedfiles/Piece.java
+++ src/sharedfiles/Piece.java
@@ -1,29 +1,31 @@
 package sharedfiles;
 
 public abstract class Piece {
-	
+
 	private boolean color;
-	private String id; 
-	public Piece(boolean b, String id){
-		this.color=b; //True is White, Black is False
+	private String id;
+
+	public Piece(boolean b, String id) {
+		this.color = b; // True is White, Black is False
 		this.id = id;
 	}
-	public boolean getColor(){
+
+	public boolean getColor() {
 		return color;
 	}
-	public boolean setColor(boolean b){
+
+	public boolean setColor(boolean b) {
 		color = b;
 		return b;
 	}
 
-	public String toString(){
+	public String toString() {
 		String x;
-		if (color==false){
+		if (color == false) {
 			x = "B";
-		}
-		else x = "W";
-	return x+id; 
+		} else
+			x = "W";
+		return x + id;
 	}
 
 }
-
