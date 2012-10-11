9b8c79231a370c8fb58dd8691fb3f5e0be07e779
sammathews <sammathews@192.168.0.142>
2012-10-10 19:24:45 -0600

    Did some testing on movablePoints(). It looks like the movablePoints method just doesn't work, if you follow the testing I've done in the driver. The code in movable points looks solid, so I'm almost certain that the issue lies with pieceCanMoveToPosition(). That must always return false, which means no points are being added to an array that is then passed back. We need to re-write this. Look over my tests and let me know if you guys disagree.

--- src/DerpyAI/Driver.java
+++ src/DerpyAI/Driver.java
@@ -1,4 +1,7 @@
 package DerpyAI;
+import java.util.ArrayList;
+import java.awt.Point; 
+
 
 public class Driver {
 
@@ -9,7 +12,11 @@ public class Driver {
 		
 		DerpyBoard db = new DerpyBoard();
 		DerpyAI aiOne = new DerpyAI(true);
-
+		ArrayList<Point> test = new ArrayList<Point>();
+		test.add(new Point(4,5));
+		test.add(new Point(5,6));
+		System.out.println("Array List 1: " + test);
+		System.out.println("Array List 2: " + aiOne.movablePoints(db.getBoardArray()[5][6]));
 		db = aiOne.randomMove();
 
 		db.printBoard();
