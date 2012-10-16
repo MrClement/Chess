54a907a3788080a60743527a84ff934b07fc608d
sammathews <sammathews@192.168.0.142>
2012-10-15 20:34:30 -0600

    Re-wrote driver to just test the randomMove() method.

--- src/DerpyAI/Driver.java
+++ src/DerpyAI/Driver.java
@@ -10,7 +10,6 @@ public class Driver {
 		
 		//October 7 2012: This needs to stay like this, folks! An AI is created, it's passed board, makes a move, returns the board, and prints it
 		
-		
 		DerpyBoard db = new DerpyBoard();
 		DerpyAI aiOne = new DerpyAI(true);
 		db = aiOne.randomMove();
