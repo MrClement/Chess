4b38ab235959b0f0e4df44ba52e862cd8d9a10b1
Alex Patel <Alex Patel@AlexPatel>
2012-10-29 17:14:11 -0600

    isThreatened == functional. fuck yeah.

--- src/generalChess/testClass.java
+++ src/generalChess/testClass.java
@@ -5,11 +5,9 @@ import sharedfiles.Board;
 public class testClass {
 	public static void main(String[] args) {
 		v1Bobby a=new v1Bobby(new Board(), true);
-		for (int i=0;i<a.allMoves().size();i++){
-			for(int k=0;k<a.allMoves().get(i).size();k++){
-				System.out.println(a.allMoves().get(i).get(k));
-				
-			}
+		a.move(3, 6, 3, 2);
+		for (int i=0;i<a.isThreatened(2, 2).size();i++){
+				System.out.println(a.isThreatened(2, 2).get(i));
 			
 		}
 		}
