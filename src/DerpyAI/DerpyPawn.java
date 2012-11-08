025388844f8a146ae6f992848aeaa2ba3919fd1e
Preston Mueller <skierjunk@gmail.com>
2012-11-08 10:10:13 -0700

    Removed unused code in DerpyPawn.java

--- src/DerpyAI/DerpyPawn.java
+++ src/DerpyAI/DerpyPawn.java
@@ -3,43 +3,11 @@ import java.awt.Point;
 
 public class DerpyPawn extends DerpyPiece {
 
-	private boolean moved;
-	
 	public DerpyPawn(boolean b, Point p) {
 		super(b,"P");
 		currentLocation = p; 
 		xMoveConstraint = 0;
 		yMoveConstraint = 2; //will need to be modified after its first y move
-		boolean moved = false; 
-	}
-
-	public void firstMoveMade(){
-		yMoveConstraint = 1; 
-		moved = true; 
-	}
-	//allows a pawn to take a piece (since piece-taking is diagonal)
-	public void allowToTake(){
-		xMoveConstraint=1;
-	}
-	
-	public void disallowToTake(){
-		xMoveConstraint=0; 
-	}
-	
-	public boolean changeLocation(Point p){
-		if (moved == false) {
-			if ((int)(p.getX()-currentLocation.getX())<=xMoveConstraint && (int)(p.getY()-currentLocation.getY())<=yMoveConstraint) {
-				currentLocation = p; 
-				firstMoveMade();
-				return true; 
-			}	
-			else return false;
-		}
-		else if ((int)(p.getX()-currentLocation.getX())<=xMoveConstraint && (int)(p.getY()-currentLocation.getY())<=yMoveConstraint) {
-			currentLocation = p; 
-			return true; 
-		}
-		else return false; 
 	}
 	 
 }
