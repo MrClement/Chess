fda16a0a6385c0ae4062289fdab46ef9165b5146
Max <mgreenwald13@kentdenver.org>
2012-11-09 08:30:34 -0700

    made my own test class so alex and i dont have to share

---
+++ src/generalChess/maxTestClass.java
@@ -0,0 +1,36 @@
+package generalChess;
+
+import sharedfiles.Board;
+
+import java.awt.Point;
+public class maxTestClass {
+	public static void main(String[] args) {
+		v1Bobby a=new v1Bobby(new Board(), true);
+
+		a.move(0,6,1,2);
+		a.move(1,6,2,3);
+		a.move(3,7,0,3);
+		a.printBoard();
+		System.out.println(a.numDefenders(1,2));
+		System.out.println(a.numDefenderValue(1,2));
+		a.randomMove();
+		a.printBoard();
+		a.randomMove();
+		a.printBoard();
+		a.randomMove();
+		a.printBoard();
+		a.randomMove();
+		a.printBoard();
+		a.printBoard();
+		a.randomMove();
+		a.printBoard();
+		System.out.println(a.kMovesDefending());
+		System.out.println(a.qMovesDefending());
+		System.out.println(a.pMovesDefending());
+		
+	
+		
+		System.out.println(a.allMoves());
+		}
+	
+}
