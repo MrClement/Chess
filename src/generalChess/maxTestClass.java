73b685b5b437110709642a5760e90dc5c166f95f
Max <mgreenwald13@kentdenver.org>
2012-11-12 08:08:08 -0700

    changed king to not move into check, error testing numdefenders and numdefender value

--- src/generalChess/maxTestClass.java
+++ src/generalChess/maxTestClass.java
@@ -7,18 +7,19 @@ public class maxTestClass {
 	public static void main(String[] args) {
 		v1Bobby a=new v1Bobby(new Board(), true);
 
-		//a.move(0,6,1,2);
-		//a.move(1,6,6,1);
+		a.move(2,7,5,2);
+		a.move(1,6,6,1);
 		
 		a.move(6,1,4,3);
 		a.move(2,0,2,3);
 		a.move(7,7,3,3);
 		System.out.println(a.newBestPieceToTake());
 		System.out.println(a.allMoves().get(13).get(a.takeIfPossible(a.allMoves().get(13))));
-		System.out.println(a.pMoves());
+		
 		a.printBoard();
-		System.out.println(a.numDefenders(1,2));
-		System.out.println(a.numDefenderValue(1,2));
+		System.out.println(a.numDefenders(0,7));
+		System.out.println(a.numDefenderValue(0,7));
+		System.out.println(a.rMoves());
 		a.randomMove();
 		a.printBoard();
 		a.randomMove();
