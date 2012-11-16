4523fa6ba8d3b9743c85783b4cb5d4c299cda11a
cwilson14 <cwilson14@10.80.28.73>
2012-11-16 11:04:15 -0700

    small fix to PCMTP that may just fix bishops...

--- src/DerpyAI/Driver.java
+++ src/DerpyAI/Driver.java
@@ -3,7 +3,7 @@ package DerpyAI;
 public class Driver {
 
 	public static void main(String[] args) {
-		
+
 		DerpyBoard db = new DerpyBoard();
 		DerpyAI aiOne = new DerpyAI(true);
 		for (int i = 0; i < 20; i++) {
