9ffc295b4dfa20fd0790b13f1cd4c4082045606f
Preston Mueller <skierjunk@gmail.com>
2012-11-19 09:46:32 -0700

    Print a failed pretty evidently.

--- src/DerpyAI/DerpyAITester.java
+++ src/DerpyAI/DerpyAITester.java
@@ -12,27 +12,32 @@ public class DerpyAITester extends DerpyAI {
 		DerpyAITester tester = new DerpyAITester(true);
 		
 		DerpyBoard b = new DerpyBoard();
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[0][1], new Point(0,2)))System.out.println("Test 0: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[1][1], new Point(1,2)))System.out.println("Test 1: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][1], new Point(2,2)))System.out.println("Test 2: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[3][1], new Point(3,2)))System.out.println("Test 3: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[4][1], new Point(4,2)))System.out.println("Test 4: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[5][1], new Point(5,2)))System.out.println("Test 5: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[6][1], new Point(6,2)))System.out.println("Test 6: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[7][1], new Point(7,2)))System.out.println("Test 7: Passed");
 		
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[0][1], new Point(0,3)))System.out.println("Test 8: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[1][1], new Point(1,3)))System.out.println("Test 9: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][1], new Point(2,3)))System.out.println("Test 10: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[3][1], new Point(3,3)))System.out.println("Test 11: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[4][1], new Point(4,3)))System.out.println("Test 12: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[5][1], new Point(5,3)))System.out.println("Test 13: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[6][1], new Point(6,3)))System.out.println("Test 14: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[7][1], new Point(7,3)))System.out.println("Test 15: Passed");
+		System.out.println("Pawns\n");
+		
+		System.out.print("Test 0: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[0][1], new Point(0,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 1: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[1][1], new Point(1,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 2: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][1], new Point(2,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 3: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[3][1], new Point(3,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 4: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[4][1], new Point(4,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 5: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[5][1], new Point(5,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 6: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[6][1], new Point(6,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 7: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[7][1], new Point(7,2))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		
+		System.out.print("Test 8: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[0][1], new Point(0,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 9: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[1][1], new Point(1,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 10: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][1], new Point(2,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 11: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[3][1], new Point(3,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 12: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[4][1], new Point(4,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 13: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[5][1], new Point(5,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 14: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[6][1], new Point(6,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 15: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[7][1], new Point(7,3))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		
+		System.out.println("\nBishops\n");
 		
 		b.getBoardArray()[3][1] = new DerpyBlank(new Point(3,1));
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][0], new Point(3,1)))System.out.println("Test 16: Passed");
-		if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][0], new Point(6,4)))System.out.println("Test 17: Passed");
+		System.out.print("Test 16: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][0], new Point(3,1))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
+		System.out.print("Test 17: "); if(tester.pieceCanMoveToPosition(b.getBoardArray()[2][0], new Point(6,4))){System.out.println("Passed");} else {System.out.println("FAILED!  <<<");}
 		
 	}
 	
