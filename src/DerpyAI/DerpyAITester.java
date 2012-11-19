7f030860d29d878bb3bc4433c054072d4d32b9b5
Preston Mueller <skierjunk@gmail.com>
2012-11-19 09:17:31 -0700

    Added DerpyAITester and make some methods protected so we can mess with them from the tester subclass.

---
+++ src/DerpyAI/DerpyAITester.java
@@ -0,0 +1,17 @@
+package DerpyAI;
+
+public class DerpyAITester extends DerpyAI {
+
+	public static void main(String[] args) {
+		DerpyAITester tester = new DerpyAITester(true);
+		
+		
+	}
+	
+	public DerpyAITester(Boolean b) {
+		super(b);
+	}
+	
+	
+
+}
