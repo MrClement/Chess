0a4187de17f5c387208b9481976306357a76306a
cwilson14 <cwilson14@10.80.10.50>
2012-12-03 09:23:25 -0700

    made a change to ChangeLocation in DerpyPiece that makes it so kings aren't confused about their positions when they castle.

--- src/DerpyAI/DerpyKing.java
+++ src/DerpyAI/DerpyKing.java
@@ -1,14 +1,14 @@
 package DerpyAI;
-import java.awt.Point; 
+
+import java.awt.Point;
 
 public class DerpyKing extends DerpyPiece {
 
 	public DerpyKing(boolean b, Point p) {
-		super(b,"K");
-		currentLocation = p; 
+		super(b, "K");
+		currentLocation = p;
 		xMoveConstraint = 1;
 		yMoveConstraint = 1;
 	}
-
 
 }
