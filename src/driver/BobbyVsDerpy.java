1147fc6e50cca9236bc992803a6d734770b7e2ca
MrClement <aclement@kentdenver.org>
2012-12-07 13:23:59 -0700

    Made BobbyVsDerpy

--- src/driver/BobbyVsDerpy.java
+++ src/driver/BobbyVsDerpy.java
@@ -1,12 +1,50 @@
 package driver;
 
+import generalChess.v1Bobby;
+import sharedfiles.Board;
+import DerpyAI.DerpyAI;
+import DerpyAI.DerpyBoard;
+
 public class BobbyVsDerpy {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public BobbyVsDerpy() {
+		Board b = new Board();
+		b.printBoard();
+		System.out.println();
+		DerpyBoard db = new DerpyBoard(b);
+		db.printBoard();
+		System.out.println();
+
+		System.out.println("Derpy:");
+		DerpyAI aiOne = new DerpyAI(true);
+		db = aiOne.makeMove(db);
+		b = db.boardEquiv();
+		b.printBoard();
+		System.out.println();
+
+		v1Bobby bobby = new v1Bobby(b, false);
+		System.out.println("Bobby:");
+		b.setBoardArray(bobby.turn(b.getBoardArray()));
+		b.printBoard();
+		// long start;
+		for (int i = 0; i < 100; i++) {
+			// start = System.currentTimeMillis();
+			System.out.println();
+			System.out.println("Derpy:");
+			db = new DerpyBoard(b);
+			db = aiOne.makeMove(db);
+			b = db.boardEquiv();
+			b.printBoard();
+			System.out.println();
+			System.out.println("Bobby:");
+			b.setBoardArray(bobby.turn(b.getBoardArray()));
+			b.printBoard();
+
+			// float time = System.currentTimeMillis() - start;
+			// time = time / 1000F;
+			// System.out.println(time);
+
+		}
 
 	}
 
